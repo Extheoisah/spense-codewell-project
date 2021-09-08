42a6ba94603fb1b97f9c354aa13bc8322ec545cd
Theophilus <extheo2019@gmail.com>
2021-09-08 19:42:06 +0100

    finised nav for desktop and mobile

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -10,3 +10,17 @@ toggleMenu.addEventListener('click', ()=> {
 closeMenu.addEventListener('click', ()=> {
     navMenu.classList.remove("show");
 })
+
+// REMOVE MENU WHEN NAVLINK IS ACTIVE
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction () {
+    // Active link
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    // Remove nav menu
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
